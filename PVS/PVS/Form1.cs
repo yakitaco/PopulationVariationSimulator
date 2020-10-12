4ec2b5ac43457f28625ac23c9493a4ad54854206
YAKI TACO <tnknet21@gmail.com>
2020-10-12 20:10:01 +0900

    [PVS] Added map of perlin tiles.

--- PVS/PVS/Form1.cs
+++ PVS/PVS/Form1.cs
@@ -1,36 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PVS {
     public partial class Form1 : Form {
-        public Form1() {
-            InitializeComponent();
-            int x = 10;
-            int y = 10;
-            for (int i = 0; i < x; i++) {
-                for (int j = 0; j < y; j++) {
-                    Label tmpLabel = new Label();
-                    tmpLabel.BackColor = System.Drawing.SystemColors.ActiveCaption;
-                    tmpLabel.Margin = new System.Windows.Forms.Padding(3);
-                    tmpLabel.Name = $"C{i:D2}_{j:D2}";
-                    tmpLabel.Size = new System.Drawing.Size(64, 64);
-                    tmpLabel.TabIndex = 0;
-                    tmpLabel.Text = $"00 00 00\n00    00\n00    00";
-                    tmpLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-                    tmpLabel.Visible = true;
-                    tmpLabel.MouseDown += new MouseEventHandler(MasulabelClick);
-                    this.flowLayoutPanel1.Controls.Add(tmpLabel);
-                    if (j == y-1) this.flowLayoutPanel1.SetFlowBreak(tmpLabel, true);
-                }
-            }
 
+        Random rnd = new System.Random();
+
+        public Form1() {
+
+            InitializeComponent();
 
         }
 
@@ -38,10 +17,42 @@ namespace PVS {
             //((Label)sender).Text = "A" + ((Label)sender).Name;
             int x = int.Parse(((Label)sender).Name.Substring(1, 2));
             int y = int.Parse(((Label)sender).Name.Substring(4, 2));
-            //cn4_main.RenewView(x, y);
+            //cn4_main.ReyewView(x, y);
             //this.Close();
         }
 
+        private void Form1_Paint(object sender, PaintEventArgs e) {
+            // Graphicsオブジェクトの作成
+            Graphics g = this.CreateGraphics();
 
+            Perlin pl = new Perlin();
+
+            double x1_rnd = rnd.NextDouble();
+            double y1_rnd = rnd.NextDouble();
+            double z1_rnd = rnd.NextDouble();
+            double x2_rnd = rnd.NextDouble();
+            double y2_rnd = rnd.NextDouble();
+            double z2_rnd = rnd.NextDouble();
+
+            for (int i = 0; i < this.Width / 4; i++) {
+                for (int j = 0; j < this.Height /4; j++) {
+                    var c = pl.OctavePerlin(0.1*i+x1_rnd, 0.1 * j+y1_rnd, z1_rnd, 6, 0.5)/2.0 + pl.OctavePerlin(0.1 * i + x2_rnd, 0.1 * j + y2_rnd, z2_rnd, 3, 0.5) / 2.0;
+                    //c = c/1.7+ Math.Cos(((double)i * 50.0/ (double)this.Width))/Math.PI + 0.5;
+                    c = c / 0.9 - Math.Cos(((double)i * 50.0/ (double)this.Width))/Math.PI/ 5.0 - Math.Cos(((double)j * 50.0 / (double)this.Height)) / Math.PI / 5.0 -0.05f;
+                    Pen p;
+                    if (c> 0.5){
+                        p = new Pen(Color.FromArgb((int)(255 * c -100), (int)(255 * c), (int)(255 * c-100)), 4);
+                    } else {
+                        p = new Pen(Color.FromArgb((int)(155 * c), (int)(155 * c), (int)(255 * c)+100), 4);
+                    }
+                    g.DrawRectangle(p, i*4, j*4, 4, 4);
+                    p.Dispose();
+                }
+            }
+
+            // Graphicsを解放する
+            g.Dispose();
+
+        }
     }
 }
