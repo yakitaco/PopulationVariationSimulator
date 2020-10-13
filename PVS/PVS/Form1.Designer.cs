5d00bd876f80df489ad59092e9974fa63c89ec83
yaki_taco <yaki_taco@yahoo.co.jp>
2020-10-13 22:33:10 +0900

    [PVS] Add config module and map module.

--- PVS/PVS/Form1.Designer.cs
+++ PVS/PVS/Form1.Designer.cs
@@ -23,22 +23,46 @@
         /// コード エディターで変更しないでください。
         /// </summary>
         private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(911, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1578, 944);
+            this.ClientSize = new System.Drawing.Size(1578, 544);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.Text = "PVS";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
---
+++ PVS/PVS/conf.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PVS {
+
+    class conf {
+        Dictionary<string, dynamic> paramList = new Dictionary<string, dynamic>();
+        private const string delimiter = "=";
+
+        public conf(string fileName) {
+            paramList.Add("height", "300");
+            paramList.Add("width", "1000");
+            paramList.Add("player1", "0");
+            paramList.Add("player2", "1");
+            paramList.Add("Xcells", "7");
+            paramList.Add("Ycells", "5");
+            paramList.Add("saveLog", "0");
+            load(fileName);
+        }
+
+        //コンフィグロード
+        private void load(string fileName) {
+            System.IO.StreamReader cfg_sr = null;
+            try {
+                cfg_sr = new System.IO.StreamReader(@fileName,
+                                System.Text.Encoding.GetEncoding("utf-8"));
+                int rc;
+
+                /* 内容を一行ずつ読み込む */
+                string cfg_str;
+                while ((rc = cfg_sr.Peek()) > -1) {
+                    cfg_str = cfg_sr.ReadLine();
+                    /* コメント行 */
+                    if (cfg_str.StartsWith("#")) {
+                        /* 何もしない */
+                    } else {
+                        /* パラメータリストをチェック＆登録 */
+                        int position = cfg_str.IndexOf(delimiter);
+                        if (position > 0) {
+                            string key = cfg_str.Substring(0, position);
+                            if (paramList.ContainsKey(key)) {
+                                paramList[key] = cfg_str.Substring(position + 1);
+                            } else {
+                                Console.WriteLine("Key not found :" + key);
+                            }
+                        } else {
+                            Console.WriteLine("Parse error :" + cfg_str);
+                        }
+                    }
+                }
+            } catch (System.IO.FileNotFoundException ex) {
+                //FileNotFoundExceptionをキャッチした時
+            } catch (System.UnauthorizedAccessException ex) {
+                //UnauthorizedAccessExceptionをキャッチした時
+            } finally {
+                if (cfg_sr != null) {
+                    cfg_sr.Dispose();
+                }
+            }
+        }
+
+        /* 文字列型パラメータ取得 */
+        public string getPrmStr(string prm) {
+            if (paramList.ContainsKey(prm)) {
+                return paramList[prm].ToString();
+            } else {
+                return null;
+            }
+        }
+
+        /* 数値型パラメータ取得 */
+        public int? getPrmInt(string prm) {
+            if (paramList.ContainsKey(prm)) {
+                return int.Parse(paramList[prm]);
+            } else {
+                return null;
+            }
+        }
+
+    }
+}
