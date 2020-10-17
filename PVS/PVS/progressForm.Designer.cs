646e2187c9d0232b4ea8b2ed98b4a58bf01c785a
yaki_taco <yaki_taco@yahoo.co.jp>
2020-10-17 23:06:50 +0900

    [PVS]Add progress form.

---
+++ PVS/PVS/progressForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace PVS {
+    partial class progressForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(302, 30);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(239, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Cancel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // progressForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 126);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "progressForm";
+            this.Text = "progressForm";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.progressForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ PVS/PVS/progressForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PVS {
+    public partial class progressForm : Form {
+        int max = 0;
+        int val = 0;
+        public bool cancel = false;  //中断フラグ
+
+        //mainFormオブジェクトを保持するためのフィールド
+        private static progressForm _progressFormInstance;
+
+        //mainFormオブジェクトを取得、設定するためのプロパティ
+        public static progressForm fInstance {
+            get {
+                return _progressFormInstance;
+            }
+            set {
+                _progressFormInstance = value;
+            }
+        }
+        public progressForm(int _max) {
+            InitializeComponent();
+            max = _max;
+            progressBar1.Maximum= _max;
+            label1.Text = val.ToString() + "/" + max.ToString() + " (" + (val * 100 / max).ToString() + "%)";
+        }
+
+        //マップイメージ表示用デリゲート
+        delegate void delegate1(int val);
+
+        public void SetProgVal(int _val) {
+            if (this.InvokeRequired) {
+                Invoke(new delegate1(_SetProgVal), _val);
+            } else {
+                _SetProgVal(_val);
+            }
+        }
+
+        public void _SetProgVal(int _val) {
+            //作成した画像を表示する
+            progressBar1.Value = _val;
+            val = _val;
+            label1.Text = val.ToString() + "/"+ max.ToString() + " ("+ (val*100/max).ToString() +"%)";
+        }
+
+        //表示終了用デリゲート
+        delegate void delegate2();
+
+        public void CloseProg() {
+            if (this.InvokeRequired) {
+                Invoke(new delegate2(_CloseProg));
+            } else {
+                _CloseProg();
+            }
+        }
+
+        public void _CloseProg() {
+            this.Close();
+        }
+
+        private void progressForm_FormClosing(object sender, FormClosingEventArgs e) {
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            cancel = true;
+            MessageBox.Show("中断しました", "progressForm",
+            MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            pvs_main.finishFlg = true; // メイン終了
+            this.Close();
+        }
+
+
+
+    }
+}
