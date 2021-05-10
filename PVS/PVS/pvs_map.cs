24e6bdd08053d1fcf05f7858f2d33fe63f231fa0
yaki_taco <yaki_taco@yahoo.co.jp>
2021-05-10 23:55:23 +0900

    [PVS]Add source of the river.

--- PVS/PVS/pvs_map.cs
+++ PVS/PVS/pvs_map.cs
@@ -18,8 +18,8 @@ namespace PVS {
     }
 
     class mapCell {
-        int height;     //高さ (0=水面)
-        CELLTYPE type;  //セルの種類
+        public int height;     //高さ (0=水面)
+        public CELLTYPE type;  //セルの種類
     }
 
     //マップイメージ画像
@@ -44,6 +44,7 @@ namespace PVS {
         public mapImage img;
         int Xcells;
         int Ycells;
+        int riverMax = 100;
 
         public pvs_map(int x, int y) {
             mapData = new mapCell[x, y];
@@ -75,11 +76,16 @@ namespace PVS {
                 Application.Run(progressForm.fInstance); // フォーム
             });
 
+            
+            // 地形の生成
             for (int i = 0; i < Xcells; i++) {
                 for (int j = 0; j < Ycells; j++) {
+                    mapData[i, j] = new mapCell();
                     var c = pl.OctavePerlin(0.1 * i + x1_rnd, 0.1 * j + y1_rnd, z1_rnd, 6, 0.5) / 2.0 + pl.OctavePerlin(0.1 * i + x2_rnd, 0.1 * j + y2_rnd, z2_rnd, 3, 0.5) / 2.0;
+                    
                     //c = c / 0.9 - Math.Cos(((double)i * 50.0 / (double)this.Width)) / Math.PI / 5.0 - Math.Cos(((double)j * 50.0 / (double)this.Height)) / Math.PI / 5.0 - 0.05f;
                     c = Math.Min(Math.Max(c / 0.5 - 0.6 - Math.Cos((double)i / (double)Xcells * Math.PI * 4.0) / 4.0 - Math.Cos((double)j / (double)Ycells * Math.PI * 4.0) / 4.0, 0.0), 1.0);
+                    mapData[i, j].height = (int)(c * 100) - 50;
                     Pen p;
                     if (c > 0.5) {
                         p = new Pen(Color.FromArgb((int)(255 * c - 100 + rnd.NextDouble() * 3), (int)(255 * c - rnd.NextDouble() * 3), (int)(255 * c - 100 + rnd.NextDouble() * 3)), 4);
@@ -95,6 +101,20 @@ namespace PVS {
                     return false;
                 }
                 progressForm.fInstance.SetProgVal(i);
+            }
+
+            // 気候の生成
+
+            // 川の生成
+            for (int i = 0; i < riverMax; i++) {
+                int x = rnd.Next(0, Xcells);
+                int y = rnd.Next(0, Ycells);
+                if (mapData[x,y].height > 0) {
+                    Pen p;
+                    p = new Pen(Color.FromArgb((int)(155 + rnd.NextDouble() * 3), (int)(155 + rnd.NextDouble() * 3), (int)(155 - rnd.NextDouble() * 3) + 100), 4);
+                    g.DrawRectangle(p, x * 4, y * 4, 4, 4);
+                    p.Dispose();
+                }
             }
 
             progressForm.fInstance.CloseProg();
