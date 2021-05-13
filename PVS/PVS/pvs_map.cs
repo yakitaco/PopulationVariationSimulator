9bd5958c3c64cc87040f1be384d1557792aeac2b
YAKI TACO <tnknet21@gmail.com>
2021-05-13 23:56:17 +0900

    [PVS]Add stream of rivers.

--- PVS/PVS/pvs_map.cs
+++ PVS/PVS/pvs_map.cs
@@ -39,6 +39,16 @@ namespace PVS {
 
     }
 
+    class pos {
+        public int x;
+        public int y;
+        public pos(int _x, int _y) {
+            x = _x;
+            y = _y;
+        }
+
+    }
+
     class pvs_map {
         mapCell[,] mapData;
         public mapImage img;
@@ -52,6 +62,7 @@ namespace PVS {
             Ycells = y;
         }
 
+        pos[] uPos = new pos[] { new pos(-1, -1), new pos(-1, 0), new pos(-1, 1), new pos(0, -1), new pos(0, 1), new pos(1, -1), new pos(1, 0), new pos(1, 1) };
         public bool make() {
             //200x100サイズのImageオブジェクトを作成する
             img = new mapImage(Xcells, Ycells);
@@ -76,13 +87,13 @@ namespace PVS {
                 Application.Run(progressForm.fInstance); // フォーム
             });
 
-            
+
             // 地形の生成
             for (int i = 0; i < Xcells; i++) {
                 for (int j = 0; j < Ycells; j++) {
                     mapData[i, j] = new mapCell();
                     var c = pl.OctavePerlin(0.1 * i + x1_rnd, 0.1 * j + y1_rnd, z1_rnd, 6, 0.5) / 2.0 + pl.OctavePerlin(0.1 * i + x2_rnd, 0.1 * j + y2_rnd, z2_rnd, 3, 0.5) / 2.0;
-                    
+
                     //c = c / 0.9 - Math.Cos(((double)i * 50.0 / (double)this.Width)) / Math.PI / 5.0 - Math.Cos(((double)j * 50.0 / (double)this.Height)) / Math.PI / 5.0 - 0.05f;
                     c = Math.Min(Math.Max(c / 0.5 - 0.6 - Math.Cos((double)i / (double)Xcells * Math.PI * 4.0) / 4.0 - Math.Cos((double)j / (double)Ycells * Math.PI * 4.0) / 4.0, 0.0), 1.0);
                     mapData[i, j].height = (int)(c * 100) - 50;
@@ -92,7 +103,7 @@ namespace PVS {
                     } else {
                         p = new Pen(Color.FromArgb((int)(155 * c + rnd.NextDouble() * 3), (int)(155 * c + rnd.NextDouble() * 3), (int)(255 * c - rnd.NextDouble() * 3) + 100), 4);
                     }
-                    g.DrawRectangle(p, i * 4, j * 4, 4, 4);
+                    g.DrawRectangle(p, i * 4, j * 4, 2, 2);
                     p.Dispose();
                 }
                 if (progressForm.fInstance.cancel == true) {
@@ -111,11 +122,28 @@ namespace PVS {
             for (int i = 0; i < riverMax; i++) {
                 int x = rnd.Next(0, Xcells);
                 int y = rnd.Next(0, Ycells);
-                if (mapData[x,y].height > 0) {
+
+                while (mapData[x, y].height > 0) {
+                    mapData[x, y].type = CELLTYPE.River;
                     Pen p;
                     p = new Pen(Color.FromArgb((int)(155 + rnd.NextDouble() * 3), (int)(155 + rnd.NextDouble() * 3), (int)(155 - rnd.NextDouble() * 3) + 100), 4);
-                    g.DrawRectangle(p, x * 4, y * 4, 4, 4);
+                    g.DrawRectangle(p, x * 4, y * 4, 2, 2);
                     p.Dispose();
+
+                    // 下流へ流れる
+                    pos pn = new pos(0, 0);
+                    int min = 99999;
+                    foreach (var cp in uPos) {
+                        if ((mapData[x + cp.x, y + cp.y].height < min) && (mapData[x + cp.x, y + cp.y].type != CELLTYPE.River)) {
+                            pn = cp;
+                            min = mapData[x + cp.x, y + cp.y].height;
+                        }
+                    }
+                    if ((pn.x == 0) && (pn.y == 0)) {
+                        break;
+                    }
+                    x += pn.x;
+                    y += pn.y;
                 }
             }
 
